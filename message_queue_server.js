8fadd5ddfb12acea5a07c9c7e9e9de4c06adc60f
Markus Winkler <tony.seing@gmail.com>
2012-08-23 15:49:47 -0400

    server starts and connected to redis

--- message_queue_server.js
+++ message_queue_server.js
@@ -1,38 +1,28 @@
-const PORT = 8000;
-const HOST = 'localhost';
+const message_server_port = 8000;
+const redis_host = '127.0.0.1';
+const redis_port = 6379;
 
 var express = require('express');
+var redis = require('redis');
+var socket = require('socket.io');
+var server = express();
+var io = socket.listen(server);
 
-// var app = module.exports = express.createServer();
-var app = express();
+// start express server listening
+server.listen(message_server_port, function() {
+    console.log('Express server started');
+    
+});
+
+var redisClient = redis.createClient(redis_port, redis_host);
+
+redisClient.on('ready', function() {
+	console.log('Redis client connected to server ' + redis_host + ':' + redis_port);	
+});
 
 
-app.use(express.staticProvider(__dirname + '/public'));
+io.sockets.on('connection', function (socket) {
+	socket.emit('hello', 'Hi there, this is server');
+});	
 
-const redis = require('redis');
-const client = redis.createClient();
 
-const io = require('socket.io');
-
-if (!module.parent) {
-    app.listen(PORT, HOST);
-    console.log("Express server listening on port %d", app.address().port)
-
-    const socket  = io.listen(app);
-
-    socket.on('connection', function(client) {
-        const subscribe = redis.createClient();
-        subscribe.subscribe('pubsub');
-        
-        subscribe.on("message", function(channel, message) {
-            client.send(message);
-        });
-
-        client.on('message', function(msg) {
-        });
-
-        client.on('disconnect', function() {
-            subscribe.quit();
-        });
-    });
-}
